27897f1b1f163a8ac06721bca1c2e048b1368825
Derrick <derrickrlewis@gmail.com>
2016-08-15 03:38:00 -0500

    Rejected commit: Default

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,17 @@
 package com.company;
-
-
-
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Main {
 
+    NewItem Pizza = new NewItem();
+    NewItem Shirt = new NewItem();
+    NewItem Tire = new NewItem();
+    NewItem Cellphone = new NewItem();
+    NewItem Paint = new NewItem();
 
     static Scanner scanner = new Scanner(System.in);
     public static ArrayList<String> stockList = new ArrayList<>();
-
 
     public static void main(String[] args) throws Exception {
 
@@ -20,16 +19,12 @@ public class Main {
         System.out.println("WELCOME TO INVENTORY");
         System.out.println("Please choose one of the following\n");
 
-
-
         while (true) {
             System.out.println("\n1|| Add new item to inventory\n");
             System.out.println("2||Remove item from inventory\n");
             System.out.println("3||Change the quantity of an item\n");
             System.out.println("4||Print List");
             String option = scanner.nextLine();
-
-
 
             if (option.equals("1")) {
                 createItem();
@@ -41,13 +36,10 @@ public class Main {
                 String option3 = scanner.nextLine();
                 System.out.println(option3 + " will be removed from the inventory");
 
-
             } else if (option.equals("3")) {
                 System.out.println("Enter the name and the qty of the item that you want to update:\n");
                 String option4 = scanner.nextLine();
-
                 System.out.println(option4 + " will be changed");
-
 
             } else if (option.equals("4")) {
                 System.out.println(stockList);
@@ -57,53 +49,45 @@ public class Main {
         }
     }
 
-
-
-
     private static void listView() {
         String name = "User";
         int items = stockList.size();
-        String.format("Hello, %s! You have %s items in inventory!!.", name, items);
+        String.format("Hello, %s! You have %s items in inventory!!", name, items);
         System.out.printf("Hello, %s! You have %s items in inventory!!.", name, items);
     }
 
     public static NewItem createItem() throws Exception {
-
-
-
-
         System.out.println("What is the name of the item that you want to add to the inventory\n");
-
         String nameItem = scanner.nextLine();
         stockList.add(nameItem);
 
-
         System.out.println("How many are adding?");
         String itemAmount = scanner.nextLine();
-
         stockList.add(itemAmount);
-
 
         System.out.println("Choose a category for your item :\n");
         System.out.println("1)Automotive 2)Food 3)Electronics 4)Clothing");
         String itemCategory = scanner.nextLine();
+        String itemCategoryChoice;
+
+
 
         NewItem returnObject = new NewItem(nameItem,itemAmount,itemCategory);
 
         if (itemCategory.equalsIgnoreCase("1")) {
-           returnObject.setCategory("Automotive");
-            stockList.add(returnObject.category);
+        itemCategoryChoice ="Automotive";
+            stockList.add(itemCategoryChoice);
         } else if (itemCategory.equalsIgnoreCase("2")) {
-            returnObject.setCategory("Food");
-            stockList.add(returnObject.category);
+            itemCategoryChoice ="Food";
+            stockList.add(itemCategoryChoice);
 
         } else if (itemCategory.equalsIgnoreCase("3")) {
-            returnObject.setCategory("Electronics");
-            stockList.add(returnObject.category);
+            itemCategoryChoice ="Electronics";
+            stockList.add(itemCategoryChoice);
 
         } else if (itemCategory.equalsIgnoreCase("4")) {
-            returnObject.setCategory("Clothing");
-            stockList.add(returnObject.category);
+            itemCategoryChoice ="Clothing";
+            stockList.add(itemCategoryChoice);
 
         } else {
             throw new Exception("Invalid Option.");
@@ -111,7 +95,5 @@ public class Main {
         }
 
         return returnObject;
-
-
     }
 }
